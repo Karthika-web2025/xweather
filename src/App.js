b7cd4603dfe7b5fef6bafccd9bacb5be49bb5b0c
Karthikagayu <106004276+Karthikagayu@users.noreply.github.com>
2026-02-03 19:41:41 +0530

    xweather

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSearch = () => {
+    if (!city) {
+      setError("Please enter a city name");
+      return;
+    }
+
+    setLoading(true);      
+    setError("");
+    setWeather(null);
+
+    fetch(
+      `https://api.weatherapi.com/v1/current.json?key=c43d778a3c174e2d81c131723260302&q=${city}`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("City not found");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setWeather(data);
+        setError("");
+      })
+      .catch((err) => {
+        console.error(err);
+        setError("Unable to fetch weather data");
+        alert("Failed to fetch weather data");
+      })
+      .finally(() => {
+        setLoading(false);  
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
+
+      <input
+        type="text"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        placeholder="Enter city name"
+      />
+      <button onClick={handleSearch} class="btn btn-success">Search</button>
+
+       {loading && <p>Loading data…</p>}
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      {weather && (
+        <div className="weather-cards">
+          <div className="weather-card">
+            <h4>Temperature</h4>
+            <p>{weather.current.temp_c} °C</p>
+          </div>
+
+          <div className="weather-card">
+            <h4>Humidity</h4>
+            <p>{weather.current.humidity} %</p>
+          </div>
+
+          <div className="weather-card">
+            <h4>Condition</h4>
+            <p>{weather.current.condition.text}</p>
+          </div>
+
+          <div className="weather-card">
+            <h4>Wind Speed</h4>
+            <p>{weather.current.wind_kph} km/h</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
